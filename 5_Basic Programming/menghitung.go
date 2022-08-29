fa556f3140adf61e766b89ebfdba1f8512a8d97d
aldimas07 <aldimasjaya.07@gmail.com>
2022-08-29 19:11:51 +0700

    problem 1 Basic Programming

---
+++ 5_Basic Programming/menghitung.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+func main(){
+	tabung()
+	// var pi float64 = 3.14
+	// var inputr float64
+	// var inputt float64
+	// var res float64
+
+	// fmt.Println("Masukkan jari jari dan tinggi tabung: ")
+	// fmt.Scanf("%f", &inputr, &inputt)
+	// res = (2*pi*inputr*inputr) + (2*pi*inputr*inputt)
+	// fmt.Println(res)
+
+}
+
+func tabung(){
+	//Problem 1
+	var T float64 = 20
+	var pi float64 = 3.14
+	var r float64 = 4
+	var res float64
+
+	res = (2*pi*r*r) + (2*pi*r*T)
+	fmt.Println(res)
+}
