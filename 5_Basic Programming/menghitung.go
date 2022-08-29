21d9007d4f2b7acd9c212824d91c6d3805b98a4d
aldimas07 <aldimasjaya.07@gmail.com>
2022-08-29 22:57:25 +0700

    Problem 2 gradenilai()

--- 5_Basic Programming/menghitung.go
+++ 5_Basic Programming/menghitung.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 func main(){
 	// tabung()
-	var pi float64 = 3.14
-	var inputr float64
-	var inputt float64
-	var res float64
+	gradenilai()
+	// var pi float64 = 3.14
+	// var inputr float64
+	// var inputt float64
+	// var res float64
 
-	fmt.Println("Masukkan jari jari lalu enter dan masukkan tinggi tabung: ")
-	fmt.Scanf("%f\n%f", &inputr, &inputt)
-	res = (2*pi*inputr*inputr) + (2*pi*inputr*inputt)
-	fmt.Println(res)
+	// fmt.Println("Masukkan jari jari lalu enter dan masukkan tinggi tabung: ")
+	// fmt.Scanf("%f\n%f", &inputr, &inputt)
+	// res = (2*pi*inputr*inputr) + (2*pi*inputr*inputt)
+	// fmt.Println(res)
 
 }
 
@@ -24,4 +25,31 @@ func tabung(){
 
 	res = (2*pi*r*r) + (2*pi*r*T)
 	fmt.Println(res)
+}
+
+func gradenilai(){
+	var studentScore int 
+	var nama string
+
+	fmt.Println("Masukkan nama siswa dan nilai siswa: ")
+	fmt.Scanf("%s\n%d", &nama, &studentScore)
+	
+	if studentScore >= 80 && studentScore <=100{
+		fmt.Println("Nama Siswa: ", nama)
+		fmt.Println("Nilai Siswa: A")
+	} else if studentScore >= 65 && studentScore <= 79 {
+		fmt.Println("Nama Siswa: ", nama)
+		fmt.Println("Nilai Siswa: B")
+	} else if studentScore >= 50 && studentScore <=64 {
+		fmt.Println("Nama Siswa: ", nama)
+		fmt.Println("Nilai Siswa: C")
+	} else if studentScore >= 35 && studentScore <=49 {
+		fmt.Println("Nama Siswa: ", nama)
+		fmt.Println("Nilai Siswa: D")
+	} else if studentScore >= 0 && studentScore <= 34 {
+		fmt.Println("Nama Siswa: ", nama)
+		fmt.Println("Nilai Siswa: E")
+	} else {
+		fmt.Println("Nilai Invalid")
+	}
 }
