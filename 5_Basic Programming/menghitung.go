10b4a15e19c3c8a2c657187cd55e387f32efb850
aldimas07 <aldimasjaya.07@gmail.com>
2022-08-30 13:19:41 +0700

    Problem 4 - Bilangan Prima

--- 5_Basic Programming/menghitung.go
+++ 5_Basic Programming/menghitung.go
@@ -1,11 +1,19 @@
 package main
 
 import "fmt"
-func main(){
+
+func main() {
 	// tabung()
 	// gradenilai()
-	faktorbilangan()
+	// faktorbilangan()
 
+	fmt.Println(primeNumber(11))
+	fmt.Println(primeNumber(13))
+	fmt.Println(primeNumber(17))
+	fmt.Println(primeNumber(20))
+	fmt.Println(primeNumber(35))
+
+	// menghitung luas permukaan tabung
 	// var pi float64 = 3.14
 	// var inputr float64
 	// var inputt float64
@@ -18,35 +26,35 @@ func main(){
 
 }
 
-func tabung(){
+func tabung() {
 	//Problem 1
 	var T float64 = 20
 	var pi float64 = 3.14
 	var r float64 = 4
 	var res float64
 
-	res = (2*pi*r*r) + (2*pi*r*T)
+	res = (2 * pi * r * r) + (2 * pi * r * T)
 	fmt.Println(res)
 }
 
-func gradenilai(){
+func gradenilai() {
 	//Problem 2
-	var studentScore int 
+	var studentScore int
 	var nama string
 
 	fmt.Println("Masukkan nama siswa dan nilai siswa: ")
 	fmt.Scanf("%s\n%d", &nama, &studentScore)
-	
-	if studentScore >= 80 && studentScore <=100{
+
+	if studentScore >= 80 && studentScore <= 100 {
 		fmt.Println("Nama Siswa: ", nama)
 		fmt.Println("Nilai Siswa: A")
 	} else if studentScore >= 65 && studentScore <= 79 {
 		fmt.Println("Nama Siswa: ", nama)
 		fmt.Println("Nilai Siswa: B")
-	} else if studentScore >= 50 && studentScore <=64 {
+	} else if studentScore >= 50 && studentScore <= 64 {
 		fmt.Println("Nama Siswa: ", nama)
 		fmt.Println("Nilai Siswa: C")
-	} else if studentScore >= 35 && studentScore <=49 {
+	} else if studentScore >= 35 && studentScore <= 49 {
 		fmt.Println("Nama Siswa: ", nama)
 		fmt.Println("Nilai Siswa: D")
 	} else if studentScore >= 0 && studentScore <= 34 {
@@ -57,19 +65,40 @@ func gradenilai(){
 	}
 }
 
-func faktorbilangan(){
-	var n int 
+func faktorbilangan() {
+	var n int
 	fmt.Print("Input: ")
 	fmt.Scanf("%d", &n)
 	fmt.Println("Output: ")
-	if n > 1{
+	if n > 1 {
 		for i := 1; i <= n; i++ {
-			if n % i == 0 {
+			if n%i == 0 {
 				fmt.Println(i)
 			}
 		}
 	} else {
 		fmt.Println("Angka harus lebih besar dari 1")
 	}
-	
+
+}
+
+func primeNumber(number int) bool {
+	var bagi int
+
+	for i := 1; i <= number; i++ {
+		if number%i == 0 {
+			bagi = bagi + 1
+		}
+	}
+
+	if bagi == 2 {
+		fmt.Print("Bilangan Prima\n")
+		return true
+
+	} else {
+		fmt.Print("Bukan Bilangan Prima\n")
+		return false
+
+	}
+
 }
