39671159bdd6fcdfe97874e583cf277621315d0f
aldimas07 <aldimasjaya.07@gmail.com>
2022-09-10 23:57:15 +0700

    Add SS Output Problem 1 dan praktikum.go

---
+++ 8_Recursive Number Theory Sorting Searching/praktikum.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	// "sort"
+)
+
+func main() {
+	// searching()
+
+	fmt.Println(primeX(1))
+	fmt.Println(primeX(5))
+	fmt.Println(primeX(8))
+	fmt.Println(primeX(9))
+	fmt.Println(primeX(10))
+
+	// fmt.Println()
+}
+
+func primeX(number int) int {
+	if number == 1 {
+		return 2
+	} else if number < 1 {
+		return 0
+	}
+
+	var jumlah int = 1
+	var i int = 1
+
+	for jumlah <= number{
+		if cekBilPrima(i){
+			jumlah++
+		}
+		i++
+	}
+	return i-1
+
+}
+
+
+func cekBilPrima(number int) bool {
+	var bagi int
+	if number == 1 {
+		return false
+	} else if number%2 == 0 {
+		return false
+	}
+
+	for i := 1; i <= int(math.Sqrt(float64(number))); i++ {
+
+		if number%i == 0 {
+			bagi = bagi + 1
+		}
+	}
+
+	if bagi == 2 {
+		return false
+	} else {
+		return true
+	}
+
+}
+// func fibonacci(number int) int {
+
+// }
+
+// func searching() bool {
+// 	element := []int{11, 22, 55, 33, 44, 12, 54, 66}
+// 	nilai := 66
+
+// 	pencarian := sort.SearchInts(element, nilai)
+
+// 	if nilai == element[pencarian] {
+// 		res := true
+// 		fmt.Println("Ketemu", element[pencarian], res)
+// 		return res
+// 	} else {
+// 		fmt.Println("Tidak ketemu", element[pencarian])
+// 		res := false
+// 		return res
+// 	}
+// }
---
+++ 8_Recursive Number Theory Sorting Searching/summary.md
@@ -0,0 +1,14 @@
+# Recursive NumberTheory Sorting Searchings
+
+## Recursive
+### 2 Bagian utama yaitu: base case dan recurrence relation
+### function yang dapat memanggil dirinya sendiri
+### Rekursive sama dengan iteratif for loop
+### Untuk menyelesaikan masalah besar dengan dipecah menjadi kecil kecil
+
+## Number theory
+### cabang matematika yang mempelajari integer
+### seperti bil prima, faktorial
+
+## sort.SearchInts berfungsi mencari nilai dengan tipe data integer didalam slice / array.
+## sort lainnya: len(), swap(), less()
