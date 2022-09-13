9931170a6a326b59d7ad1989a3a334840cf5cf56
aldimas07 <aldimasjaya.07@gmail.com>
2022-09-13 12:24:07 +0700

    Add SS Output Problem 3 & Source code

--- 8_Recursive Number Theory Sorting Searching/praktikum.go
+++ 8_Recursive Number Theory Sorting Searching/praktikum.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math"
-
 	// "sort"
 )
 
@@ -16,11 +15,15 @@ func main() {
 	// fmt.Println(primeX(9))
 	// fmt.Println(primeX(10))
 
-	fmt.Println(fibonacci(0))
-	fmt.Println(fibonacci(2))
-	fmt.Println(fibonacci(9))
-	fmt.Println(fibonacci(10))
-	fmt.Println(fibonacci(12))
+	// fmt.Println(fibonacci(0))
+	// fmt.Println(fibonacci(2))
+	// fmt.Println(fibonacci(9))
+	// fmt.Println(fibonacci(10))
+	// fmt.Println(fibonacci(12))
+
+	primaSegiEmpat(2, 3, 13)
+	primaSegiEmpat(5, 2, 1)
+	
 }
 
 func primeX(number int) int {
@@ -48,12 +51,11 @@ func cekBilPrima(number int) bool {
 	var bagi int
 	if number == 1 {
 		return false
-	} else if number%2 == 0 {
+	} else if number%2 == 0 && number > 2 {
 		return false
 	}
 
 	for i := 1; i <= int(math.Sqrt(float64(number))); i++ {
-
 		if number%i == 0 {
 			bagi = bagi + 1
 		}
@@ -75,6 +77,34 @@ func fibonacci(number int) int {
 	return fibonacci(number-1) + fibonacci(number-2)
 
 }
+
+func cekStartPrime(start int) int {
+		if cekBilPrima(start) {
+			return start
+		}
+		return cekStartPrime(1 + start)
+
+}
+
+func primaSegiEmpat(high, wide, start int){
+	var total int
+
+	for i := 0; i < wide; i++ {
+
+		for j := 0; j < high; j++ {
+			start = cekStartPrime(start + 1)
+			total = total + start
+
+			fmt.Print(start)
+			fmt.Print(" ")
+		}
+		fmt.Println()
+	}
+	fmt.Print(total)
+
+	fmt.Print("\n\n")
+}
+
 
 // func searching() bool {
 // 	element := []int{11, 22, 55, 33, 44, 12, 54, 66}
